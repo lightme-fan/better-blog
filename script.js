f3eec4e48972f61fb417b46198f610246c67ca01
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-11 09:21:59 +0100

    added new blog post

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('it works');
+
+const postList = document.getElementById('post-list');
+const submitButton = document.querySelector('.btn-primary');
+const deleteButton = document.querySelector('.btn-delete');
+
+const newCard = document.createElement('div');
+newCard.classList.add('card');
+console.log(newCard);
+
+const newImage = document.createElement('img');
+newImage.src = '';
+newImage.width = '';
+newImage.height = '';
+newImage.alt  = '';
+newImage.classList.add('card-img-top');
+
+
+const newCardBody = document.createElement('div');
+newCardBody.classList.add('card-body');
+
+const newHeading = document.createElement('h5');
+newHeading.classList.add('card-title');
+
+const newText = document.createElement('p');
+newText.classList.add('card-text');
+
